4926031dcf7ff81705b8c5e0e4c0551e0b5b4ede
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-20 21:35:19 +0500

    Update: File Structure

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"Musify"
+using namespace std;
+
+// Main Function
+int main() {
+    Musify m;
+    m.makePlayList({"song1.mp3"});
+    string firstSong="song1.mp3";
+    m.play(firstSong);
+    return 0;
+}
